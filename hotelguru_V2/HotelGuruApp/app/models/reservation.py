3035ca08d86aaee2437fe8ae875cb2788f658053
Zambo Gergely <zgergely0217@gmail.com>
2025-03-23 18:42:39 +0100

    foglalás

--- hotelguru_V2/HotelGuruApp/app/models/reservation.py
+++ hotelguru_V2/HotelGuruApp/app/models/reservation.py
@@ -4,6 +4,7 @@ from sqlalchemy.types import Integer, Date
 from sqlalchemy import ForeignKey
 from typing import List
 from datetime import date
+from sqlalchemy.exc import IntegrityError
 
 class Reservation(db.Model):
     __tablename__ = "reservations"
@@ -24,17 +25,46 @@ class Reservation(db.Model):
         return f"Reservation(id={self.id!r}, start_date={self.start_date!r}, end_date={self.end_date!r}, reservation_date={self.reservation_date!r})"
 
     def create_reservation(user_id: int, room_id: int, start_date: date, end_date: date) -> Reservation:
-            new_reservation = Reservation(
-                user_id = user_id,
-                room_id = room_id,
-                start_date = start_date,
-                end_date = end_date,
-                reservation_date = date_today()
-                )
+        # Ellenőrzés: start_date < end_date
+        if start_date >= end_date:
+            raise ValueError("A kezdő dátum nem lehet korábban mint a vég dátum")
+
+        # Ellenőrzés: user_id és room_id léteznek-e
+        user = db.session.get(User, user_id)
+        room = db.session.get(Room, room_id)
+        if not user:
+            raise ValueError(f"Felhasználó a következő id-val nem létezik: {user_id} ")
+        if not room:
+            raise ValueError(f"Szoba a következő id-val nem létezik: {room_id} ")
+
+        # Új foglalás létrehozása
+        new_reservation = Reservation(
+            user_id=user_id,
+            room_id=room_id,
+            start_date=start_date,
+            end_date=end_date,
+            reservation_date=date.today()
+        )
+
+        try:
             db.session.add(new_reservation)
             db.session.commit()
-            return new_reservation
+        except IntegrityError:
+            db.session.rollback()
+            raise ValueError("Nem sikerült létrehozni a foglalást.")
+
+        return new_reservation
 
 #példa hívásra:
-#new_reservation = create_reservation(user_id =1, room_id=101, start_date =date(2025, 4, 3))
-#print(new_reservation)
+#from datetime import date
+
+##try:
+#    new_reservation = create_reservation(
+#        user_id=1,
+#        room_id=101,
+#        start_date=date(2025, 4, 3),
+#        end_date=date(2025, 4, 10)
+#    )
+#    print(new_reservation)
+#except ValueError as e:
+#   print(f"Hiba: {e}")
